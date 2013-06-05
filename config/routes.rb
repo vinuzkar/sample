86cc18006f0fee99d2585bcffb89d80688024535
V1nz <vinz.geass@yahoo.com>
2013-06-05 09:51:29 +0700

    Add SSL in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Sample::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
 
@@ -7,6 +7,7 @@ Sample::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@ require 'spec_helper'
 describe User do
 
   before do
-  @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -41,6 +41,16 @@ describe User do
   describe "when e-mail is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
+  end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
   end
   
   describe "when email format is invalid" do
